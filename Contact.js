8987a578f41a053c4f537bc56b1bf07261ea0e8b
dghousi <dawlatzai.ghousi@gmail.com>
2023-01-04 14:42:24 +0430

    Add contact form with its component

---
+++ Contact.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Contact = props => {
+  return (
+    <div className="contact-card">
+      <img src={props.img} />
+      <h3>{props.name}</h3>
+      <div className="info-group">
+        <img src="./images/phone-icon.png" />
+        <p>{props.phone}</p>
+      </div>
+      <div className="info-group">
+        <img src="./images/mail-icon.png" />
+        <p>{props.email}</p>
+      </div>
+    </div>
+  )
+}
+
+export default Contact
